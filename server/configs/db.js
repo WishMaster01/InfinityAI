37f006b76e7dcd139b16ed7bccb154ad5e819dc2
WishMaster01 <yadavsumit2589@gmail.com>
2026-09-22 23:20:02 +0530

    Updated prisma configuration and fixed version prisma error.

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,10 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neon } from "@neondatabase/serverless";
+
+const sql = neon(process.env.DATABASE_URL);
+const adapter = new PrismaNeon(sql);
 
 const globalForPrisma = globalThis;
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
